87ea2bf1999a19ea395b2db0547742597063451e
zhenyar123 <70575666+zhenyar123@users.noreply.github.com>
2021-04-16 20:13:08 +0300

    Update invite.js

--- commandgif/invite.js
+++ commandgif/invite.js
@@ -10,7 +10,7 @@ const embed = new Discord.MessageEmbed()
 .setColor(`RANDOM`)
 .setThumbnail(message.author.avatarURL({dynamic: "true"}))
 .setTitle(`**invite**`)
-.setDescription(`[Click here](https://discord.com/oauth2/authorize?client_id=806840212608909344&permissions=70346817&scope=bot)`)
+.setDescription(`[Click here](https://discord.com/api/oauth2/authorize?client_id=832663600606347345&permissions=8&scope=bot)`)
 .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))    
 message.channel.send({embed});
 }
